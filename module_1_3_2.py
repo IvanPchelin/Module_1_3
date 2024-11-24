ee5430d76058689eced924edf14af8eb8b1788e4
IvanPchelin <IvanPCH737@gmail.com>
2024-11-24 18:14:21 +0400

    Add files via upload

---
+++ module_1_3_2.py
@@ -0,0 +1,14 @@
+example = "Ivan"
+print("Hello " + example)
+print(example [0])
+print(example [-1])
+name = 'Urban'
+print(name [-3:])
+print(name [::-1])
+example = 'Топинамбур'
+print(example [1:10:2])
+print(example [0])
+print(example [-1])
+print(example [-5:])
+print(example [::-1])
+print(example [1:10:2])
