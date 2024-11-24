e47a9f863f0513d8f6bd3f539591cdb39ec90414
IvanPchelin <IvanPCH737@gmail.com>
2024-11-24 18:15:28 +0400

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,12 @@
+food = ["apple", "coconut", "banana"]
+print(food[0])
+food[0] = "peach"
+print(food)
+food.append(True)
+print(food)
+food.extend("sting")
+print(food)
+food.extend(["sting", 2])
+print(food)
+food.remove("coconut")
+print(food)
