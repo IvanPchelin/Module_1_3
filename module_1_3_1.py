b0ddb76372de3ea1a236cae21e52689ac280934c
IvanPchelin <IvanPCH737@gmail.com>
2024-11-24 18:13:23 +0400

    Add files via upload

---
+++ module_1_3_1.py
@@ -0,0 +1,10 @@
+name = 'Количество выполненных ДЗ = 12'
+print(name)
+The_number_of_completedDZ = 12
+name = 'Количество затраченных часов = 1.5'
+print(name)
+Number_of_hours_spent = 1.5
+name = 'Название курса = Python'
+print(name)
+print('Время на одно задание =',(Number_of_hours_spent / The_number_of_completedDZ))
+print('Курс: Python,' ' всего задач:12,'' затрачено часов:1.5, среднее время выполнения:',Number_of_hours_spent / The_number_of_completedDZ,'часа.')
